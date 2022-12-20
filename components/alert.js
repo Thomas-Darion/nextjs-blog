48e921c49e87272196a686676759699ccf3ada0b
Thomas-Darion <micromaniac.toma17@hotmail.fr>
2022-12-20 11:28:17 +0100

    first commit

---
+++ components/alert.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './alert.module.css';
+import clsx from 'clsx';
+
+export default function Alert({ children, type }) {
+  return (
+    <div
+      className={clsx({
+        [styles.success]: type === 'success',
+        [styles.error]: type === 'error',
+      })}
+    >
+      {children}
+    </div>
+  );
+}
